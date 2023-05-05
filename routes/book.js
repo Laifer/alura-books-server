f657bbbbac4a0e9ed6aea2afc2fb74f5e461a203
Laifer <gabriel.alves@cotefacil.com>
2023-05-05 10:27:07 -0300

    feat: now using the controller that we created in the activity 5 from class 2 and ending the activity

--- routes/book.js
+++ routes/book.js
@@ -1,16 +1,9 @@
 const { Router } = require('express')
-
 const router = Router()
 
-router.get('/', (req, res) => {
-    try {
-        throw new Error('teste')
-        res.send('Ola mundo!!!!!')
-    } catch (error) {
-        res.status(500)
-        res.send(error.message)
-    }
-})
+const { getBooks } = require('../controllers/book')
+
+router.get('/', getBooks)
 
 router.post('/', (req, res) => {
     res.send(`${req.content}`)
