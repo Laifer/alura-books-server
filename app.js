5d147b213787b52a908d98f91a2cce914a7252d7
Laifer <gabriel.alves@cotefacil.com>
2023-05-08 11:43:50 -0300

    feat: on the class of the fullstack course, we added 'cors' to allow the API to be read from any origin, otherwise the React app can't get the data from it

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routeBook = require('./routes/book')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({origin: "*"}))
 
 app.use('/books', routeBook)
 
